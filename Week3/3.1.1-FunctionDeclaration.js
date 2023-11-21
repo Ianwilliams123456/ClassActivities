e95bd572587b6d2b2e77941b944f9521508ad97e
Ian Williams <ianwilliams123456@gmail.com>
2023-11-20 17:27:29 -0800

    Function Declaration

--- Week3/3.1.1-FunctionDeclaration.js
+++ Week3/3.1.1-FunctionDeclaration.js
@@ -16,8 +16,8 @@ function threeModTwo() {
   console.log(3 % 2);
 }
 //function expression
-/*let age = 18;
-const checkDrivingAge = function() {
+let age = 18;
+/*const checkDrivingAge = function() {
   if (age >= 16) {
     console.log(true);
   } else {
